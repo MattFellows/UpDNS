c1a8da51ffb48b58cda647fba427bd01d72428d7
Matt Fellows <matt.fellows@bespokesoftware.com>
2017-08-27 17:35:59 +0100

    Fix to generate a random sessionId wiht the Ping library

--- checkers/ping.js
+++ checkers/ping.js
@@ -5,12 +5,13 @@ function Ping(addr) {
     self.address = addr;
     self.result = false;
     self.session = null;
+    self.sessionId = (process.pid + Math.ceil(Math.random() * 65535) % 65535)
 
     var options = {
         networkProtocol: ping.NetworkProtocol.IPv4,
         packetSize: 16,
         retries: 1,
-        sessionId: (process.pid % 65535),
+        sessionId: self.sessionId,
         timeout: 500,
         ttl: 128
     };
@@ -31,9 +32,9 @@ Ping.prototype.check = check;
 module.exports = Ping;
 
 var works = new Ping('192.168.0.1');
-var fails = new Ping('192.168.33.1');
+var fails = new Ping('192.168.37.71');
 setTimeout(function() {
-    var fails2 = new Ping('192.168.33.1');
-}, 1000);
+    var fails2 = new Ping('192.168.37.71');
+}, 3000);
 
 //works.setup();
