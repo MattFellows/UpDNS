2becdace4beb8c5d2bc34070ae6a91c3e8dfec2b
Matt Fellows <github@matt-fellows.me.uk>
2017-08-29 17:35:05 +0100

    eparated out config

--- checkers/ping.js
+++ checkers/ping.js
@@ -1,13 +1,13 @@
 var ping = require ("net-ping");
 
-function Ping(addr) {
+function Ping(addr, options) {
     var self = this;
     self.address = addr;
     self.result = false;
     self.session = null;
     self.sessionId = (process.pid + Math.ceil(Math.random() * 65535) % 65535)
 
-    var options = {
+    var localOptions = {
         networkProtocol: ping.NetworkProtocol.IPv4,
         packetSize: 16,
         retries: 1,
@@ -15,20 +15,20 @@ function Ping(addr) {
         timeout: 500,
         ttl: 128
     };
-    self.session = ping.createSession(options);
-    self.interval = setInterval(function() { asyncCheck(self); }, 30000);
+    self.session = ping.createSession(localOptions);
+    self.interval = setInterval(function() { asyncCheck(self); }, options.interval || 30000);
     asyncCheck(self)
 }
 
 function asyncCheck(self) {
     self.session.pingHost(self.address, function(error, target, sent, rcvd) {
         self.result = !error
-        //console.log("Just checked: " + self.address + " recieved: " + self.result);
+        console.log("Just checked: " + self.address + " recieved: " + self.result);
     });
 }
 
 function check() {
-    //console.log("Checked: " + this.address + " recieved: " + this.result);
+    console.log("Checked: " + this.address + " recieved: " + this.result);
     if (!this.interval) {
         this.interval = setInterval(asyncCheck, 30000);
     }
--- updns.js
+++ updns.js
@@ -2,11 +2,11 @@ var dnsd = require('dnsd');
 var named = require('node-named');
 var server = dnsd.createServer(requestHandler);
 server.listen(5300, '127.0.0.1');
-console.log('Server running at 127.0.0.1:5300')
+console.log('Server running at 127.0.0.1:5300');
 
 var CheckedRecord = require('./recordchecker.js');
-var Ping = require('./checkers/ping.js')
-var records = [];
+var Ping = require('./checkers/ping.js');
+var records = require('./records.json');
 
 function requestHandler(req, res) {
   var question = res.question[0]
@@ -27,13 +27,42 @@ function requestHandler(req, res) {
   }
 }
 
+function setupRecordCheckers() {
+  for (var domainName in records.domains) {
+    var domain = records.domains[domainName];
+    for (var recordTypeName in domain.records) {
+      var recordType = domain.records[recordTypeName];
+      var checkerType = recordType.checker.type;
+      var checkerFrequency = recordType.checker.frequency;
+      recordType.checkedRecords = [];
+      for (var recordIndex in recordType.values) {
+        var record = recordType.values[recordIndex];
+          switch (checkerType) {
+          case 'ping': 
+            var DNSrecord = getRecordType(recordTypeName, record);
+            var checkedRecord = new CheckedRecord(DNSrecord, new Ping(record, recordType.checker));
+            recordType.checkedRecords.push(checkedRecord);
+        }
+      }
+    }
+  }
+}
+
+function getRecordType(recordType, record) {
+  switch (recordType) {
+    case 'A':
+      return new named.ARecord(record);
+  }
+}
+
 function getRecord(query) {
   var domain = query.question[0].name;
   var type = query.question[0].type;
 
   console.log("Finding: " + type + " records for domain: " + domain);
-  var allRecordsForDomain = records[domain];
-  var checkedRecordsForFomain = allRecordsForDomain.filter(function(item) {
+  var allRecordsForDomainAndType = records.domains[domain].records[type].checkedRecords;
+
+  var checkedRecordsForFomain = allRecordsForDomainAndType.filter(function(item) {
     var checkResult = item.checkRecord();
     return checkResult;
   });
@@ -49,15 +78,23 @@ function getRecord(query) {
   if (sortedCheckedRecordsForDomain.length > 0) {
     var target = sortedCheckedRecordsForDomain[0];
     target.incrementLoad();
+    for (var i = 0; i < sortedCheckedRecordsForDomain.length; i++) {
+      if (sortedCheckedRecordsForDomain[i] != target) {
+        sortedCheckedRecordsForDomain[i].decrementLoad();
+      }
+    }
     var record = target.getRecord();
     return record;
   }
   return null;
 }
 
+setupRecordCheckers();
+/*
 if (!records['example.com']) {
   records['example.com'] = [];
 }
 records['example.com'].push(new CheckedRecord(new named.ARecord('192.168.0.1'), new Ping('192.168.0.1')));
 records['example.com'].push(new CheckedRecord(new named.ARecord('192.168.0.2'), new Ping('192.168.0.2')));
 records['example.com'].push(new CheckedRecord(new named.ARecord('192.168.0.197'), new Ping('192.168.0.197')));
+*/
